f688aa92b31a1857bd9d0cc33a21c886982c76aa
huweitao <devilsnevercry@163.com>
2019-01-18 11:48:14 +0800

    [M]Make HSwiftTools into other repo

--- HSwiftTools/HSwiftTools/OCDevTools/HSwiftTools-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-#ifndef HSwiftTools_Bridging_Header_h
-#define HSwiftTools_Bridging_Header_h
-
-#import "UnSeedManager.h"
-
-#endif
